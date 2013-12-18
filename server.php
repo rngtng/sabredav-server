b498699bc151212963688a1c25528d5617f4e96a
Tobias Bielohlawek <tobi@soundcloud.com>
2013-12-18 22:56:29 +0100

    fix htaccess, add groupware server

--- server.php
+++ server.php
@@ -1,37 +1,55 @@
 <?php
 
-use Sabre\DAV;
-
 date_default_timezone_set('UTC');
+
+require_once 'config.php';
 
 require_once 'vendor/autoload.php';
 
-$u = 'tobi';
-$p = '1234';
+$baseUri = '/dav.warteschlange.de';
+$_SERVER['REQUEST_URI'] =  $baseUri.$_SERVER['REQUEST_URI'];
 
-$auth = new \Sabre\HTTP\DigestAuth();
-$auth->init();
+$pdo = new PDO(MYSQL_HOST, MYSQL_USER, MYSQL_PASS);
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-if ($auth->getUsername() != $u || !$auth->validatePassword($p)) {
-    $auth->requireLogin();
-    echo "Authentication required\n";
-    die();
+function exception_error_handler($errno, $errstr, $errfile, $errline ) {
+    echo $errstr;
+        throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
 }
+set_error_handler("exception_error_handler");
 
-$rootDirectory = new DAV\FS\Directory('data');
+$authBackend      = new \Sabre\DAV\Auth\Backend\PDO($pdo);
+$principalBackend = new \Sabre\DAVACL\PrincipalBackend\PDO($pdo);
+$carddavBackend   = new \Sabre\CardDAV\Backend\PDO($pdo);
+$caldavBackend    = new \Sabre\CalDAV\Backend\PDO($pdo);
 
-$server = new DAV\Server($rootDirectory);
-$server->setBaseUri('/');
+$nodes = array(
+    // /principals
+    new \Sabre\CalDAV\Principal\Collection($principalBackend),
+    // /calendars
+    new \Sabre\CalDAV\CalendarRootNode($principalBackend, $caldavBackend),
+    // /addressbook
+    new \Sabre\CardDAV\AddressBookRoot($principalBackend, $carddavBackend),
+
+    //new Sabre\DAV\FS\Directory('public'),
+);
+
+// The object tree needs in turn to be passed to the server class
+$server = new \Sabre\DAV\Server($nodes);
+$server->setBaseUri($baseUri);
+
+// Plugins
+$server->addPlugin(new \Sabre\DAV\Auth\Plugin($authBackend, 'WebDav'));
+$server->addPlugin(new \Sabre\DAV\Browser\Plugin());
+$server->addPlugin(new \Sabre\CalDAV\Plugin());
+$server->addPlugin(new \Sabre\CardDAV\Plugin());
+$server->addPlugin(new \Sabre\DAVACL\Plugin());
 
 // The lock manager is reponsible for making sure users don't overwrite each others changes. Change 'data' to a different
 // directory, if you're storing your data somewhere else.
-$lockBackend = new DAV\Locks\Backend\File('data/locks');
-$lockPlugin = new DAV\Locks\Plugin($lockBackend);
-$server->addPlugin($lockPlugin);
-
-$plugin = new DAV\Browser\Plugin();
-$server->addPlugin($plugin);
-
+// $lockBackend = new \Sabre\DAV\Locks\Backend\File('data/locks');
+// $lockPlugin = new \Sabre\DAV\Locks\Plugin($lockBackend);
+// $server->addPlugin($lockPlugin);
 
 $server->exec();
 
