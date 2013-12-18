0388586ba8e832c8348e80a0106b8efc0c7a9b4e
Tobias Bielohlawek <tobi@soundcloud.com>
2013-12-18 01:10:10 +0100

    update readme, added .htaccess

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-sabredav-server
-===============
+# Sabredav-Server
 
 Custom SabreDav Server
+
+# Install
+Follow instructions on:
+https://code.google.com/p/sabredav/wiki
+
+# Usage
+
--- server.php
+++ server.php
@@ -3,32 +3,27 @@
 use
     Sabre\DAV;
 
-    // Files we need
+require_once 'vendor/autoload.php';
 
-    require_once 'vendor/autoload.php';
+// Now we're creating a whole bunch of objects
 
-    // Now we're creating a whole bunch of objects
+// Change public to something else, if you are using a different directory for your files
+$rootDirectory = new DAV\FS\Directory('public');
 
-    // Change public to something else, if you are using a different directory for your files
-    $rootDirectory = new DAV\FS\Directory('public');
+// The server object is responsible for making sense out of the WebDAV protocol
+$server = new DAV\Server($rootDirectory);
+$server->setBaseUri('/'); // ideally, SabreDAV lives on a root directory with mod_rewrite sending every request to server.php
 
-    // The server object is responsible for making sense out of the WebDAV protocol
-    $server = new DAV\Server($rootDirectory);
+// The lock manager is reponsible for making sure users don't overwrite each others changes. Change 'data' to a different
+// directory, if you're storing your data somewhere else.
+$lockBackend = new DAV\Locks\Backend\File('data/locks');
+$lockPlugin = new DAV\Locks\Plugin($lockBackend);
+$server->addPlugin($lockPlugin);
 
-    // If your server is not on your webroot, make sure the following line has the correct information
+$plugin = new DAV\Browser\Plugin();
+$server->addPlugin($plugin);
 
-    // $server->setBaseUri('/~evert/mydavfolder'); // if its in some kind of home directory
-    // $server->setBaseUri('/dav/server.php/'); // if you can't use mod_rewrite, use server.php as a base uri
-    // $server->setBaseUri('/'); // ideally, SabreDAV lives on a root directory with mod_rewrite sending every request to server.php
+// All we need to do now, is to fire up the server
+$server->exec();
 
-    // The lock manager is reponsible for making sure users don't overwrite each others changes. Change 'data' to a different 
-    // directory, if you're storing your data somewhere else.
-    $lockBackend = new DAV\Locks\Backend\File('data/locks');
-    $lockPlugin = new DAV\Locks\Plugin($lockBackend);
-
-    $server->addPlugin($lockPlugin);
-
-    // All we need to do now, is to fire up the server
-    $server->exec();
-
-	    ?>
+?>
